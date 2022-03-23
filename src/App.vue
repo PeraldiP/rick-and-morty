26e43e07333a70c44ece51815f6426b0ac66c9b3
Peraldi <p.peraldi@epmistes.net>
2022-03-24 00:15:17 +0100

    Formulaire recherche avec result

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,104 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex justify-center items-center">
+    <img src="./assets/rick_and_morty.png" class="w-28 h-28 m-8" />
+    <h1 class="font-black text-2xl underline decoration-red-700">
+      Welcome on our site of Rick and Morty
+    </h1>
+  </div>
+  <div class="bg-red-500 flex justify-center items-center">
+    You search a character(s) of Rick and Morty, fill out the form !
+  </div>
+  <div class="flex items-center justify-center">
+    <form class="flex items-center justify-start m-8 flex-col bg-slate-200 w-1/3 p-5 rounded-xl" v-on:submit.prevent="update">
+      <h2 class="underline text-2xl">Formulaire :</h2>
+      <div class="h-36 flex flex-col justify-around">
+        <div>
+          <label class="mr-1" for="nameInput">Character Name</label>
+          <input
+            class="text-center"
+            v-model="name"
+            type="text"
+            name="nameCharacter"
+            id="nameInput"
+            placeholder="Name Character"
+          />
+        </div>
+        <div>
+          <label class="mr-1" id="status">Status</label>
+          <select v-model="status" name="status" id="status">
+            <option value="">Défault</option>
+            <option value="alive">Alive</option>
+            <option value="dead">Dead</option>
+            <option value="unknown">Unknown</option>
+          </select>
+        </div>
+        <div>
+          <label class="mr-1" id="gender">Gender</label>
+          <select v-model="gender" name="gender" id="gender">
+            <option value="">Défault</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="genderless">Genderless</option>
+            <option value="unknown">Unknown</option>
+          </select>
+        </div>
+      </div>
+      <button @click="searchCharacters()" class=" bg-sky-600 rounded-lg p-3">
+        Find Characters
+      </button>
+    </form>
+  </div>
+  <Character msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Character from "./components/Character.vue";
+import "./assets/tailwind.css";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Character,
+  },
+  data: function () {
+    return {
+      name: "",
+      status: "",
+      gender: "",
+    };
+  },
+  methods: {
+    searchCharacters() {
+      const client = new ApolloClient({
+        uri: "https://rickandmortyapi.com/graphql",
+        cache: new InMemoryCache(),
+      });
+      client
+              .query({
+                query: gql`
+                  query Query {
+                    characters(filter: {name:"${this.name}", status:"${this.status}", gender:"${this.gender}"}) {
+                      results {
+                        id
+                        name
+                        gender
+                        status
+                        image
+                        species
+                      }
+                    }
+                  }
+                `,
+              })
+              .then((result) => {
+                this.$store.state.characters=result.data.characters.results;
+                console.log(result.data.characters.results)
+                }
+              );
+          },
+        },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/Character.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="">
+    <div class="underline text-lg font-bold text-center"><span class="bg-orange-700 p-2 rounded-lg">Résults :</span></div>
+    <div v-if="this.$store.state.characters" class="flex flex-wrap justify-center align-center">
+      <div  v-for="character in $store.state.characters" :key="character.id" class="bg-slate-700 m-4 p-4 w-52 flex flex-col justify-center items-center text-white">
+        <div class="underline">{{character.name}}</div>
+        <img :src="character.image" class="w-28 h-28 mb-2 mt-2">
+        <div><span class="underline">Gender</span> :{{character.gender}}</div>
+        <div><span class="underline">Species</span> :{{character.species}}</div>
+        <div class="text-sm"><span class="underline ">Status</span> :{{character.status}}</div>
+      </div>    
+    </div> 
+    <div class="text-center mt-10" v-else> Not results</div> 
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
